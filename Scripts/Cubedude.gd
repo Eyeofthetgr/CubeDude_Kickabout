a92f85d30a108751777e701b6e98fd192acbcee0
Eyeofthetgr <kamil2000-22@o2.pl>
2020-04-27 22:13:34 +0200

    Detecting and correcting errors, adding shadows, importing files from godot 3.1.2 to godot 3.0.2

--- Scripts/Cubedude.gd
+++ Scripts/Cubedude.gd
@@ -1,13 +1,48 @@
 extends KinematicBody
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var motion = Vector3()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export var speed = 10
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+const UP = Vector3(0,1,0)
+const GRAVITY = -5
+
+func _physics_process(delta):
+	move()
+	fall()
+
+
+func move():
+	if Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
+		motion.z = -speed
+	elif Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
+		motion.z = speed
+	else: 
+		motion.z = 0
+		
+	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = -speed
+	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = speed
+	else: 
+		motion.x = 0
+		
+	move_and_slide(motion, UP)
+
+
+func fall():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y = GRAVITY
+
+
+
+
+
+
+
+
+
+
+
